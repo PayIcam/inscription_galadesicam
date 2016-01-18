fb1f3781285ffd76d88ee3ec2afeb70c35b8dc86
Antoine Giraud <antoine.giraud@outlook.com>
2016-01-18 02:26:00 +0100

    fix app not logged in

--- src/middleware.php
+++ src/middleware.php
@@ -12,7 +12,10 @@ $app->add(function ($request, $response, $next) {
             // Il n'était pas encore connecté en tant qu'icam.
             $this->flash->addMessage('info', "Vous devez être connecté pour accéder au reste de l'application");
             return $response->withStatus(303)->withHeader('Location', $this->router->pathFor('about'));
-        }else if(!empty($status->user) && isset($status->application->app_url) && strpos($status->application->app_url, 'inscription_gala') === false){ // il était connecté en tant qu'icam mais l'appli non
+        }
+    }
+    if (!empty($status->user)){
+        if (empty($status->application) || isset($status->application->app_url) && strpos($status->application->app_url, 'inscription_gala') === false){ // il était connecté en tant qu'icam mais l'appli non
             try {
                 $payutcClient->loginApp(array("key"=>$this->get('settings')['PayIcam']['payutc_key']));
                 $status = $payutcClient->getStatus();
@@ -21,18 +24,16 @@ $app->add(function ($request, $response, $next) {
                 return $response->withStatus(303)->withHeader('Location', $this->router->pathFor('about'));
             }
         }
-        if (!empty($status->user)){
-            // tout va bien
+        // tout va bien
+        $gingerUserCard = $gingerClient->getUser($Auth->getUserField('email'));
+        if (empty($gingerUserCard)) { // l'utilisateur n'avait jamais été ajouté à Ginger O.o
             $gingerUserCard = $gingerClient->getUser($Auth->getUserField('email'));
-            if (empty($gingerUserCard)) { // l'utilisateur n'avait jamais été ajouté à Ginger O.o
-                $gingerUserCard = $gingerClient->getUser($Auth->getUserField('email'));
-            }if (empty($gingerUserCard)) { // l'utilisateur n'a pas un mail icam valide // on ne devrait jamais avoir cette erreur car on passe par payutc et lui a besoin d'avoir ginger qui marche ... je crois ...
-                $this->flash->addMessage('warning', "Votre Mail Icam n'est pas reconnu par Ginger...");
-                return $response->withStatus(303)->withHeader('Location', $this->router->pathFor('about'));
-            }
+        }if (empty($gingerUserCard)) { // l'utilisateur n'a pas un mail icam valide // on ne devrait jamais avoir cette erreur car on passe par payutc et lui a besoin d'avoir ginger qui marche ... je crois ...
+            $this->flash->addMessage('warning', "Votre Mail Icam n'est pas reconnu par Ginger...");
+            return $response->withStatus(303)->withHeader('Location', $this->router->pathFor('about'));
         }
     }
-
+    
     // $response->getBody()->write('BEFORE');
     $response = $next($request, $response);
     // $response->getBody()->write('AFTER');
