7eb7047b586979ac0e101918858ab928e99e195f
Guillaume Dubois <dbmgui@gmail.com>
2017-12-11 19:03:55 +0100

    Correction affichage

--- templates/home.php
+++ templates/home.php
@@ -137,7 +137,7 @@
         </ul></dd>
         <dt>Prix payé:</dt>
         <dd><?= $newResa->icamData['price']; ?> <em><small>par <?= $newResa->icamData['paiement'] ?> le <?= substr($newResa->date_option, 0, 10) ?></small></em></dd>
-        <dt>Plage horaire d'entrée :</dt>
+        <dt>Plage horaire:</dt>
         <dd><?= $newResa->icamData['plage_horaire_entrees'] ?></dd>
         <dt>Numéro de bracelet:</dt>
         <dd><?= ($newResa->icamData['bracelet_id'])?$newResa->icamData['bracelet_id']:'<em>Vous avez bien réservé votre place. Cependant, vous devez récupérer votre bracelet.</em>'; ?></dd>
@@ -200,7 +200,7 @@
         </ul></dd>
         <dt>Prix payé:</dt>
         <dd><?= $UserReservation['price']; ?> <em><small>par <?= $UserReservation['paiement'] ?> le <?= substr($UserReservation['inscription'], 0, 10) ?></small></em></dd>
-        <dt>Plage horaire d'entrée :</dt>
+        <dt>Plage horaire:</dt>
         <dd><?= corriger_horaire($UserReservation['plage_horaire_entrees']) ?></dd>
         <dt>Numéro de bracelet:</dt>
         <dd><?= ($UserReservation['bracelet_id'])?$UserReservation['bracelet_id']:'<em>Vous avez bien réservé votre place. Cependant, vous devez récupérer votre bracelet.</em>'; ?></dd>
@@ -222,7 +222,7 @@
                 </ul></dd>
                 <dt>Prix payé:</dt>
                 <dd><?= $guest['price']; ?> <em><small>par <?= $guest['paiement'] ?> le <?= substr($UserReservation['inscription'], 0, 10) ?></small></em></dd>
-                <dt>Plage horaire d'entrée :</dt>
+                <dt>Plage horaire:</dt>
                 <dd><?= corriger_horaire($guest['plage_horaire_entrees']) ?></dd>
                 <dt>Numéro de bracelet:</dt>
                 <dd><?= ($guest['bracelet_id'])?$guest['bracelet_id']:'<em>Vous avez bien réservé sa place. Cependant, vous devez récupérer votre bracelet.</em>'; ?></dd>
