15d09629afa01bac3f456a9f0be78273ec04380a
Unknown <gregoire.dervaux@2019.icam.fr>
2018-01-07 23:11:13 +0100

    ca marche sauf pour le bouton

--- src/routes.php
+++ src/routes.php
@@ -399,43 +399,29 @@ $app->post('/enreg', function($request, $response, $args){
     $participants->execute();
     $nb_participants = $participants->fetch();
 
-    var_dump($participants);
-
     $premier_creneau = $bd->prepare('SELECT COUNT(*) FROM guests WHERE plage_horaire_entrees =\'21h-21h45\'');
     $premier_creneau->execute();
     $nb_creneau1 = $premier_creneau->fetch();
-
-    var_dump($nb_creneau1);
 
     $deuxieme_creneau = $bd->prepare('SELECT COUNT(*) FROM guests WHERE plage_horaire_entrees = \'21h45-22h30\'');
     $deuxieme_creneau->execute();
     $nb_creneau2 = $deuxieme_creneau->fetch();
 
-    var_dump($nb_creneau2);
-
     $troisieme_creneau = $bd->prepare('SELECT COUNT(*) FROM guests WHERE plage_horaire_entrees = \'22h30-23h\'');
     $troisieme_creneau->execute();
     $nb_creneau3 = $troisieme_creneau->fetch();
-
-    var_dump($nb_creneau3);
 
     $quotat_creneau1 = $bd->prepare('SELECT value FROM configs WHERE name = \'quota_entree_21h_21h45\'');
     $quotat_creneau1->execute();
     $quotat1 = $quotat_creneau1->fetch();
 
-    var_dump($quotat1);
-
     $quotat_creneau2 = $bd->prepare('SELECT value FROM configs WHERE name = \'quota_entree_21h45_22h30\'');
     $quotat_creneau2->execute();
     $quotat2 = $quotat_creneau2->fetch();
 
-    var_dump($quotat2);
-
     $quotat_creneau3 = $bd->prepare('SELECT value FROM configs WHERE name = \'quota_entree_22h30_23h\'');
     $quotat_creneau3->execute();
     $quotat3 = $quotat_creneau3->fetch();
-
-    var_dump($quotat3)
      
     if ($quotat1<$nb_creneau1){  //verifie si creneau dispo
         $creneau1=false;
@@ -455,10 +441,6 @@ $app->post('/enreg', function($request, $response, $args){
         $creneau3=true;
     }
 
-    var_dump($creneau1, $creneau2, $creneau3);
-
-    var_dump($post);
-
     foreach ($post as $creneau) {
         if ($creneau == '21h-21h45' && $creneau1==false){
             $this->flash->addMessage('info', 'le premier créneau est complet');
@@ -475,15 +457,17 @@ $app->post('/enreg', function($request, $response, $args){
     }
     foreach ($post as $key => $value) {
 
+        $key=preg_replace('#[^0-9]#',"",$key);
+
         $new_creneau = $bd->prepare('UPDATE guests SET plage_horaire_entrees = :horaire WHERE id = :login ');
         $new_creneau -> bindParam('horaire', $value, PDO::PARAM_STR);
         $new_creneau -> bindParam('login', $key, PDO::PARAM_INT);
         $new_creneau->execute();
 
-    $this->flash->addMessage('info', 'Vos changements de crénaux ont bien été pris en compte.');
-        return $response->withStatus(303)->withHeader('Location', $this->router->pathFor('edit'));
     }
 
+    $this->flash->addMessage('info', 'Vos changements de crénaux ont bien été pris en compte.');
+        return $response->withStatus(303)->withHeader('Location', $this->router->pathFor('edit'));
 });
 
 $app->post('/edit', function ($request, $response, $args) {
