fbec7bd6c7a25343f75bd78067e317cc55a8a86e
Guillaume Dubois <dbmgui@gmail.com>
2018-01-08 00:39:52 +0100

    test affichage prix

--- templates/edit_reservation.php
+++ templates/edit_reservation.php
@@ -73,15 +73,14 @@ else
                         if (!is_null($num_bracelet))
                         {?>
                             <?= (isset($UserReservation['plage_horaire_entrees']) && $UserReservation['plage_horaire_entrees']) ?
-                            '<div class="checkbox">Vous avez deja réservé la plage horaire d\'entrée de '.corriger_horaire($dataPlageHoraireEntree)[$UserReservation['plage_horaire_entrees']].' et vous avez déja pris votre bracelet</div>' : ''; 
-                            echo('<input hidden name="resa[plage_horaire_entrees]" ng-model="resa.plage_horaire_entrees">');
+                            '<div class="checkbox">Vous avez deja réservé la plage horaire d\'entrée de '.corriger_horaire($dataPlageHoraireEntree)[$UserReservation['plage_horaire_entrees']].' et vous avez déja pris votre bracelet</div>' : '';
                         }
                         else
                         {?>
                         <?= (isset($UserReservation['plage_horaire_entrees']) && $UserReservation['plage_horaire_entrees']) ?
                             '<div class="checkbox">Vous avez deja réservé la plage horaire d\'entrée de '.corriger_horaire($dataPlageHoraireEntree)[$UserReservation['plage_horaire_entrees']].'</div>' : ''; ?>
 
-                        <input type="button" href="<?php $RouteHelper->getPathFor('modification_du_creneau') ?>" class="btn btn-default" value="Changer d'horaire" name="resa[plage_horaire_entrees]" ng-model="resa.plage_horaire_entrees">
+                        <input type="button" href="<?php $RouteHelper->getPathFor('modification_du_creneau') ?>" class="btn btn-default" value="Changer d'horaire">
 
                         <?php }
                     }?>
@@ -145,13 +144,14 @@ else
                                 if (!is_null($num_bracelet))
                                 {?>
                                     <?= (isset($UserGuests[$i]['plage_horaire_entrees']) && $UserGuests[$i]['plage_horaire_entrees']) ?
-                                    '<div class="checkbox">Vous avez deja réservé la plage horaire d\'entrée de '.corriger_horaire($dataPlageHoraireEntree)[$UserGuests[$i]['plage_horaire_entrees']].' et vous avez déja pris votre bracelet</div>' : '';
-                                        echo('<input hidden name="resa[plage_horaire_entrees]" ng-model="resa.plage_horaire_entrees">');
+                                    '<div class="checkbox">Vous avez deja réservé la plage horaire d\'entrée de '.corriger_horaire($dataPlageHoraireEntree)[$UserGuests[$i]['plage_horaire_entrees']].' et vous avez déja pris votre bracelet</div>' : ''; ?>
+                                        <?= $Form->select('resa[plage_horaire_entrees]', 'hidden', array('ng-model'=>'resa.plage_horaire_entrees', 'data'=>corriger_horaire($dataPlageHoraireEntreeShort)));
                                 }
                                 else
                                 {?>
                                     <?= (isset($UserGuests[$i]['plage_horaire_entrees']) && $UserGuests[$i]['plage_horaire_entrees']) ?
                             '<div class="checkbox">Vous avez deja réservé la plage horaire d\'entrée de '.corriger_horaire($dataPlageHoraireEntree)[$UserGuests[$i]['plage_horaire_entrees']].'</div>' : ''; ?>
+                                    <?= $Form->select('resa[plage_horaire_entrees]', 'hidden', array('ng-model'=>'resa.plage_horaire_entrees', 'data'=>corriger_horaire($dataPlageHoraireEntreeShort))); ?>
 
                                     <input type="button" href="<?php $lien_creneau ?>" class="btn btn-default" value="Changer d'horaire" name="resa[plage_horaire_entrees]" ng-model="resa.plage_horaire_entrees">
 
@@ -211,15 +211,16 @@ else
                                     if (!is_null($num_bracelet))
                                     {?>
                                         <?= (isset($UserGuests[$j]['plage_horaire_entrees']) && $UserGuests[$j]['plage_horaire_entrees']) ?
-                                        '<div class="checkbox">Vous avez deja réservé la plage horaire d\'entrée de '.corriger_horaire($dataPlageHoraireEntree)[$UserGuests[$j]['plage_horaire_entrees']].' et vous avez déja pris votre bracelet</div>' : '';
-                                        echo('<input hidden name="resa[plage_horaire_entrees]" ng-model="resa.plage_horaire_entrees">');
+                                        '<div class="checkbox">Vous avez deja réservé la plage horaire d\'entrée de '.corriger_horaire($dataPlageHoraireEntree)[$UserGuests[$j]['plage_horaire_entrees']].' et vous avez déja pris votre bracelet</div>' : '';?>
+
+                                        <?= $Form->select('resa[plage_horaire_entrees]', 'hidden', array('ng-model'=>'resa.plage_horaire_entrees', 'data'=>corriger_horaire($dataPlageHoraireEntreeShort)));
                                     }
                                     else
                                     {?>
                                         <?= (isset($UserGuests[$j]['plage_horaire_entrees']) && $UserGuests[$j]['plage_horaire_entrees']) ?
                                 '<div class="checkbox">Vous avez deja réservé la plage horaire d\'entrée de '.corriger_horaire($dataPlageHoraireEntree)[$UserGuests[$j]['plage_horaire_entrees']].'</div>' : ''; ?>
-
-                                        <input type="button" href="<?php $lien_creneau ?>" class="btn btn-default" value="Changer d'horaire" name="resa[plage_horaire_entrees]" ng-model="resa.plage_horaire_entrees">>
+                                        <?= $Form->select('resa[plage_horaire_entrees]', 'hidden', array('ng-model'=>'resa.plage_horaire_entrees', 'data'=>corriger_horaire($dataPlageHoraireEntreeShort))); ?>
+                                        <input type="button" href="<?php $lien_creneau ?>" class="btn btn-default" value="Changer d'horaire">
 
                                     <?php }
                                 }?>
